3e298be9c4b5723f65186e77c7bd5554bc45141d
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-12 23:45:05 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/MULTIPLY Problem 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+//multiply
+
+int main(){
+int a;
+printf("Enter a: ");
+scanf("%d", &a);
+
+int b;
+printf("Enter b: ");
+scanf("%d", &b);
+
+int c=a*b;
+printf("The product is %d",c);
+
+
+    return 0;
+}
