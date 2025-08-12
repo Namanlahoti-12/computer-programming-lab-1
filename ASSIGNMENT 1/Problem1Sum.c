750058a66117dd11c50bf96948d2c425b05f4aa3
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-12 23:41:47 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/Problem1Sum.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+//sum
+
+int main(){
+int a;
+printf("Enter a: ");
+scanf("%d", &a);
+
+int b;
+printf("Enter b: ");
+scanf("%d", &b);
+
+int c=a+b;
+printf("The sum is %d",c);
+
+
+    return 0;
+}
