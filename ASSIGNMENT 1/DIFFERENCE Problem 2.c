1a965b7d1d45c90c9915f17f86eeb5ee96e5f494
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-12 23:43:32 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/DIFFERENCE Problem 2.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+//Difference
+
+int main(){
+int a;
+printf("Enter a: ");
+scanf("%d", &a);
+
+int b;
+printf("Enter b: ");
+scanf("%d", &b);
+
+int c=a-b;
+printf("The difference is %d",c);
+
+
+    return 0;
+}
