f80c981d32dd1267a511ed5e5847d2417e06c418
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-12 23:48:38 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/Problem 5.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+
+int main(){
+float a;
+printf("Enter a: ");
+scanf("%f", &a);
+
+float b;
+printf("Enter b: ");
+scanf("%f", &b);
+
+float division=a/b;
+float sum=a+b;
+float product=a*b;
+float difference=a-b;
+
+printf("The difference is %f\n",difference);
+printf("The sum is %f\n", sum);
+printf("the product is %f\n", product);
+printf("The value of a/b is %f \n" ,division);
+
+    return 0;
+}
