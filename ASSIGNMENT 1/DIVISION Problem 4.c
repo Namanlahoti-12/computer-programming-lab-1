5911dda940f5beb7c6f6a7124eab63ab6f1fc179
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-12 23:46:12 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/DIVISION Problem 4.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+//DIVIDE
+
+int main(){
+float a;
+printf("Enter a: ");
+scanf("%f", &a);
+
+float b;
+printf("Enter b: ");
+scanf("%f", &b);
+
+float c=a/b;
+printf("The value of a/b is %f",c);
+
+
+    return 0;
+}
