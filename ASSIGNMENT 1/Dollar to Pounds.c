5e1bbaa23b47b2e9192ebd8b220ebfff12238c9d
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-13 15:51:36 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/Dollar to Pounds.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (){
+float dollar;
+printf("$: ");
+scanf("%f", &dollar);
+float rupees= dollar*48;
+float pounds= rupees/70;
+printf("Pounds = %f", pounds);
+
+return 0;
+}
