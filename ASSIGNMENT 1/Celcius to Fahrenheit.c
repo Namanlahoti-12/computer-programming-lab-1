7e1c4019c236b3eb4956f1fb301acbdcc88c5fc3
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-13 16:45:59 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/Celcius to Fahrenheit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//Celcius to fahrenheit
+int main(){
+float C;
+printf("Celcius: ");
+scanf("%f", &C);
+float x = 9;
+float z= x/5;
+float y = z*C;
+float F = y+32;
+printf("Fahrenheit: %f", F);
+return 0;
+}
