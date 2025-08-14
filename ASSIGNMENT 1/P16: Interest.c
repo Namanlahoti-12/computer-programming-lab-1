01f094228f80b474947afff105a123bf50527e91
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-14 15:52:23 +0530

    Update and rename Interest.c to P16: Interest.c

--- ASSIGNMENT 1/P16: Interest.c
+++ ASSIGNMENT 1/P16: Interest.c
@@ -4,15 +4,19 @@ int main(){
 float P;
 printf("Principle Ammount:");
 scanf("%f", &P);
+  //R= RATE OF INTEREST
 float R;
 printf("Rate of Interest: ");
 scanf("%f", &R);
+  //N= NUMBER OF YEARS
 float N;
 printf("Number of years:");
 scanf("%f", &N);
+  //I = INTEREST AMMOUNT
 float I= (P*R*N)/100;
 printf("Interest: %f ",I);
 return 0;
 
 
 }
+
