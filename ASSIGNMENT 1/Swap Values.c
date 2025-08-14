abe8220dacc2b1c3ea82fa606435ac39b83361b2
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-14 15:32:19 +0530

    Add files via upload

---
+++ ASSIGNMENT 1/Swap Values.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+float a,b, x;
+printf("Enter a: ");
+scanf("%f", &a);
+printf("Enter b: ");
+scanf("%f", &b);
+// Before swapping
+printf("Before Swapping\n");
+printf("a = %f\n", a);
+printf("b = %f\n", b);
+x= a;
+a=b;
+b=x;
+// After swapping
+printf("After Swapping\n");
+printf("a = %f\n", a);
+printf("b = %f", x);
+
+
+return 0;
+}
