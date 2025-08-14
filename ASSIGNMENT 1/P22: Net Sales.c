73924ee5535332530617c3d404c7447b1e752b72
Namanlahoti-12 <namanlahoti1024@gmail.com>
2025-08-14 15:55:33 +0530

    Update and rename Net Sales.c to P22: Net Sales.c

--- ASSIGNMENT 1/P22: Net Sales.c
+++ ASSIGNMENT 1/P22: Net Sales.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
-
 //Net Sales
-
 int main(){
-
 int gross_sales;
-
 printf("Gross Sales: ");
-
 scanf("%d",&gross_sales);
-
 //10% is given as 0.1 times
-
 float discount = 0.1*gross_sales;
-
 float net_sales= gross_sales - discount;
-
 printf("Net Sales: %f\n", net_sales);
-
 return 0;
-
 }
+
