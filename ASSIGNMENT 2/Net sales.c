23eb92751f3d6671a794e598e303a0db5d2a7579
Naman Lahoti <namanlahoti1024@gmail.com>
2025-08-15 12:58:34 +0530

    Add files via upload

---
+++ ASSIGNMENT 2/Net sales.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+//Net Salary
+int main(){
+float gross_sales;
+printf("Gross Sales: ");
+scanf("%f", &gross_sales);
+float discount;
+if(gross_sales>20000){
+//0.15 times gross sales mean 15 percent of gross sales
+discount = 0.15*gross_sales;
+}
+else if(gross_sales>10000){
+//0.1 times gross sales mean 10 percent of gross sales
+discount = 0.1*gross_sales;
+}
+else{
+//0.05 times gross sales mean 5 percent of gross sales
+discount = 0.05*gross_sales;
+}
+float net_sales = gross_sales - discount;
+
+printf("Discount: %f\n", discount);
+printf("Net Sales: %f\n", net_sales);
+return 0;
+
+
+}
