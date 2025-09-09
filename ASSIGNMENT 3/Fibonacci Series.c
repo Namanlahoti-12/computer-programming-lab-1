beb3c5be2cb4354b0cb7a8c3489eda29aa8d50dd
Naman Lahoti <namanlahoti1024@gmail.com>
2025-09-09 09:20:19 +0530

    Add files via upload

---
+++ ASSIGNMENT 3/Fibonacci Series.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int a = 1, b = 1, c;
+
+    printf("Enter how many numbers: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    printf("%d %d ", a, b);
+
+    for(i = 3; i <= n; i++) {
+        c = a + b;
+        printf("%d ", c);
+        a = b;
+        b = c;
+    }
+return 0;
+}
